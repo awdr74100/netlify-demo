28be209f95a31028497e989fbe8eca32ab1bc174
藍奕濡 <a78945612385238@gmail.com>
2020-07-31 16:26:24 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <p>before</p>
+    <p>after</p>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <router-view></router-view>
